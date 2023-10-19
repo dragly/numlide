99a50fdd86bdfa6c1c56354bb82851326ede776f
Svenn-Arne Dragly <s@dragly.com>
2023-10-19 23:09:30 +0200

    Remove duplicated code in Wrapper operations

--- src/numlide/wrapper.py
+++ src/numlide/wrapper.py
@@ -1,10 +1,21 @@
 from __future__ import annotations
 from collections.abc import Callable
+from enum import Enum, auto
 from typing import Any, Sequence, Tuple
 import halide as hl
 import numpy as np
 from dataclasses import dataclass
 from .utils import var_from_index, vars_from_shape, tr
+
+
+class _Operation(Enum):
+    add = auto()
+    sub = auto()
+    mul = auto()
+    truediv = auto()
+    floordiv = auto()
+    mod = auto()
+    pow_ = auto()
 
 
 @dataclass
@@ -44,7 +55,7 @@ class Wrapper:
         f.__setitem__(tr(left_variables), self.inner.__getitem__(tr(right_variables)))
         return Wrapper(inner=f, shape=shape)
 
-    def __add__(self, other) -> Wrapper:
+    def _perform_operation(self, other, operation: _Operation) -> Wrapper:
         if not isinstance(other, Wrapper) or isinstance(other, int) or isinstance(other, float):
             other = wrap(other)
         f = hl.Func("add")
@@ -52,121 +63,83 @@ class Wrapper:
         if isinstance(other, Wrapper):
             other_variables = vars_from_shape(other.shape)
             new_variables = variables + other_variables
-            f[new_variables] = self.inner[variables] + other.inner[other_variables]
+            match operation:
+                case _Operation.add:
+                    f[new_variables] = self.inner[variables] + other.inner[other_variables]
+                case _Operation.sub:
+                    f[new_variables] = self.inner[variables] - other.inner[other_variables]
+                case _Operation.mul:
+                    f[new_variables] = self.inner[variables] * other.inner[other_variables]
+                case _Operation.truediv:
+                    f[new_variables] = self.inner[variables] / other.inner[other_variables]
+                case _Operation.floordiv:
+                    f[new_variables] = self.inner[variables] // other.inner[other_variables]
+                case _Operation.mod:
+                    f[new_variables] = self.inner[variables] % other.inner[other_variables]
+                case _Operation.pow_:
+                    f[new_variables] = self.inner[variables] ** other.inner[other_variables]
+                case _:
+                    raise RuntimeError(f"Operation not supported: {operation}")
             shape = np.broadcast_shapes(self.shape, other.shape)
         else:
-            f[variables] = self.inner[variables] + other
+            match operation:
+                case _Operation.add:
+                    f[variables] = self.inner[variables] + other
+                case _Operation.sub:
+                    f[variables] = self.inner[variables] - other
+                case _Operation.mul:
+                    f[variables] = self.inner[variables] * other
+                case _Operation.truediv:
+                    f[variables] = self.inner[variables] / other
+                case _Operation.floordiv:
+                    f[variables] = self.inner[variables] // other
+                case _Operation.mod:
+                    f[variables] = self.inner[variables] % other
+                case _Operation.pow_:
+                    f[variables] = self.inner[variables] ** other
+                case _:
+                    raise RuntimeError(f"Operation not supported: {operation}")
             shape = self.shape
         return Wrapper(inner=f, shape=shape)
+
+    def __add__(self, other) -> Wrapper:
+        return self._perform_operation(other, _Operation.add)
 
     def __radd__(self, other) -> Wrapper:
         return self + other
 
     def __sub__(self, other) -> Wrapper:
-        if not isinstance(other, Wrapper) or isinstance(other, int) or isinstance(other, float):
-            other = wrap(other)
-        f = hl.Func("sub")
-        variables = vars_from_shape(self.shape)
-        if isinstance(other, Wrapper):
-            other_variables = vars_from_shape(other.shape)
-            new_variables = variables + other_variables
-            f[new_variables] = self.inner[variables] - other.inner[other_variables]
-            shape = np.broadcast_shapes(self.shape, other.shape)
-        else:
-            f[variables] = self.inner[variables] - other
-            shape = self.shape
-        return Wrapper(inner=f, shape=shape)
+        return self._perform_operation(other, _Operation.sub)
 
     def __rsub__(self, other) -> Wrapper:
         return wrap(other) - self
 
     def __mul__(self, other) -> Wrapper:
-        if not isinstance(other, Wrapper) or isinstance(other, int) or isinstance(other, float):
-            other = wrap(other)
-
-        f = hl.Func("mul")
-        variables = vars_from_shape(self.shape)
-        if isinstance(other, Wrapper):
-            other_variables = vars_from_shape(other.shape)
-            new_variables = variables + other_variables
-            f[new_variables] = self.inner[variables] * other.inner[other_variables]
-            shape = np.broadcast_shapes(self.shape, other.shape)
-        else:
-            f[variables] = self.inner[variables] * other
-            shape = self.shape
-        return Wrapper(inner=f, shape=shape)
+        return self._perform_operation(other, _Operation.mul)
 
     def __rmul__(self, other) -> Wrapper:
         return self * other
 
     def __truediv__(self, other) -> Wrapper:
-        if not isinstance(other, Wrapper) or isinstance(other, int) or isinstance(other, float):
-            other = wrap(other)
-        f = hl.Func("truediv")
-        variables = vars_from_shape(self.shape)
-        if isinstance(other, Wrapper):
-            other_variables = vars_from_shape(other.shape)
-            new_variables = variables + other_variables
-            f[new_variables] = self.inner[variables] / other.inner[other_variables]
-            shape = np.broadcast_shapes(self.shape, other.shape)
-        else:
-            f[variables] = self.inner[variables] / other
-            shape = self.shape
-        return Wrapper(inner=f, shape=shape)
+        return self._perform_operation(other, _Operation.truediv)
 
     def __rtruediv__(self, other) -> Wrapper:
         return wrap(other) / self
 
     def __floordiv__(self, other) -> Wrapper:
-        if not isinstance(other, Wrapper) or isinstance(other, int) or isinstance(other, float):
-            other = wrap(other)
-        f = hl.Func("floordiv")
-        variables = vars_from_shape(self.shape)
-        if isinstance(other, Wrapper):
-            other_variables = vars_from_shape(other.shape)
-            new_variables = variables + other_variables
-            f[new_variables] = self.inner[variables] // other.inner[other_variables]
-            shape = np.broadcast_shapes(self.shape, other.shape)
-        else:
-            f[variables] = self.inner[variables] // other
-            shape = self.shape
-        return Wrapper(inner=f, shape=shape)
+        return self._perform_operation(other, _Operation.floordiv)
 
     def __rfloordiv__(self, other) -> Wrapper:
         return wrap(other) // self
 
     def __mod__(self, other) -> Wrapper:
-        if not isinstance(other, Wrapper) or isinstance(other, int) or isinstance(other, float):
-            other = wrap(other)
-        f = hl.Func("mod")
-        variables = vars_from_shape(self.shape)
-        if isinstance(other, Wrapper):
-            other_variables = vars_from_shape(other.shape)
-            new_variables = variables + other_variables
-            f[new_variables] = self.inner[variables] % other.inner[other_variables]
-            shape = np.broadcast_shapes(self.shape, other.shape)
-        else:
-            f[variables] = self.inner[variables] % other
-            shape = self.shape
-        return Wrapper(inner=f, shape=shape)
+        return self._perform_operation(other, _Operation.mod)
 
     def __rmod__(self, other) -> Wrapper:
         return wrap(other) % self
 
     def __pow__(self, other) -> Wrapper:
-        if not isinstance(other, Wrapper) or isinstance(other, int) or isinstance(other, float):
-            other = wrap(other)
-        f = hl.Func("pow")
-        variables = vars_from_shape(self.shape)
-        if isinstance(other, Wrapper):
-            other_variables = vars_from_shape(other.shape)
-            new_variables = variables + other_variables
-            f[new_variables] = self.inner[variables] ** other.inner[other_variables]
-            shape = np.broadcast_shapes(self.shape, other.shape)
-        else:
-            f[variables] = self.inner[variables] ** other
-            shape = self.shape
-        return Wrapper(inner=f, shape=shape)
+        return self._perform_operation(other, _Operation.pow_)
 
     def __rpow__(self, other) -> Wrapper:
         return wrap(other) ** self
