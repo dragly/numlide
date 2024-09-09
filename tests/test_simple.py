Fix broadcast, fix structured light test

tests/test_simple.py
@@ -44,10 +44,43 @@ def test_math():
     compare(np.mean, nl.mean)
     compare(np.sum, nl.sum)
 
+
 def test_numpy():
     a = np.array([1, 2, 4])
     b = nl.array([1, 2, 4])
     np.testing.assert_equal(a + b, np.array([2, 4, 8]))
     np.testing.assert_almost_equal(np.mean(a), np.mean(b))
 
-test_numpy()
+
+def test_newaxis():
+    def add_axis(array):
+        return array[:, np.newaxis]
+
+    array = np.array([1, 2, 3])
+    result_np = add_axis(array)
+    result_nl = add_axis(nl.wrap(array))
+
+    np.testing.assert_equal(result_np, result_nl)
+
+
+def test_broadcast():
+    def multiply(array):
+        return array * np.arange(0, 4)
+
+    array = np.array(
+        [
+            [
+                [1, 2, 3, 4],
+                [5, 6, 7, 8],
+            ],
+            [
+                [2, 3, 4, 5],
+                [6, 7, 8, 9],
+            ],
+        ]
+    )
+
+    np.testing.assert_equal(
+        multiply(array),
+        multiply(nl.array(array)),
+    )

tests/test_structured_light.py
@@ -11,10 +11,13 @@ def test_structured_light():
         minimum = m.min(images, axis=2)
         maximum = m.max(images, axis=2)
         threshold = (maximum + minimum) / 2
-        binary_code = images > threshold
-        decimal_code = binary_code * 2 ** np.arange(0, 10)
+        binary_code = images > threshold[:, :, np.newaxis]
+        print(binary_code.shape)
+        decimal_code = binary_code * (2 ** np.arange(0, 10))
+        print(decimal_code.shape)
+        return decimal_code
 
     result_np = structured_light(np, images)
-    print(result_np[500, 500])
+    result_nl = structured_light(nl, images)
 
-    # np.testing.assert_allclose(result_nl.to_numpy(), result_np)
+    np.testing.assert_allclose(result_nl.to_numpy(), result_np)